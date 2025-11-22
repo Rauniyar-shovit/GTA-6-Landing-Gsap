4d993b7996fd2b82d71cb3e052b80775af9edd43
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-11-22 20:15:06 +1100

    added the lucia-life seciton

--- src/sections/SecondVideo.tsx
+++ src/sections/SecondVideo.tsx
@@ -1,40 +1,41 @@
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 
 const SecondVideo = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
   useGSAP(() => {
-    gsap.set(".lucia", {
-      opacity: 0,
-      marginTop: "-20vh",
-    });
+    gsap.set(".lucia", { marginTop: "-60vh", opacity: 0 });
 
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: "lucia",
+        trigger: ".lucia",
         start: "top top",
         end: "bottom top",
         scrub: 2,
         pin: true,
       },
     });
+
     tl.to(".lucia", { opacity: 1, duration: 1, ease: "power1.inOut" });
+
     const video = videoRef.current;
-    if (video) {
-      video.onloadedmetadata = () => {
-        tl.to(
-          video,
-          {
-            currentTime: video.duration,
-            duration: 3,
-            ease: "power1.inOut",
-          },
-          "<"
-        );
-      };
-    }
+    if (!video) return;
+
+    video.onloadedmetadata = () => {
+      tl.to(
+        video,
+        {
+          currentTime: video.duration,
+          duration: 3,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+    };
   });
+
   return (
     <section className="lucia">
       <div className="h-dvh">
@@ -45,7 +46,9 @@ const SecondVideo = () => {
           preload="auto"
           src="/videos/output2.mp4"
           className="size-full object-cover second-vd"
-          style={{ objectPosition: "15% 0%" }}
+          style={{
+            objectPosition: "15% 0%",
+          }}
         />
       </div>
     </section>
