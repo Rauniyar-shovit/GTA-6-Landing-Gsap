21a283c80bf892731010da2ab4572495c2018217
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-11-22 19:01:47 +1100

    implemented the lucia section

---
+++ src/sections/SecondVideo.tsx
@@ -0,0 +1,55 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
+const SecondVideo = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useGSAP(() => {
+    gsap.set(".lucia", {
+      opacity: 0,
+      marginTop: "-20vh",
+    });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "lucia",
+        start: "top top",
+        end: "bottom top",
+        scrub: 2,
+        pin: true,
+      },
+    });
+    tl.to(".lucia", { opacity: 1, duration: 1, ease: "power1.inOut" });
+    const video = videoRef.current;
+    if (video) {
+      video.onloadedmetadata = () => {
+        tl.to(
+          video,
+          {
+            currentTime: video.duration,
+            duration: 3,
+            ease: "power1.inOut",
+          },
+          "<"
+        );
+      };
+    }
+  });
+  return (
+    <section className="lucia">
+      <div className="h-dvh">
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+          src="/videos/output2.mp4"
+          className="size-full object-cover second-vd"
+          style={{ objectPosition: "15% 0%" }}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default SecondVideo;
